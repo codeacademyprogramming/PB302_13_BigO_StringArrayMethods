3968be2b07f4f8a13cd57926509c51e1e9298af1
eaghayev <eaghayevv@gmail.com>
2024-01-07 21:59:16 +0400

    project added

---
+++ 07012024/BuiltInMethods/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+
+namespace BuiltInMethods
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numArr = { 143, 20, 35 };
+            int[] newNumArr = new int[10];
+
+            //numArr.CopyTo(newNumArr, 2);
+            Array.Copy(numArr, 1, newNumArr, 4, 2);
+            Array.Clear(newNumArr, 0, 10);
+
+
+         
+            Array.Sort(numArr);
+            Array.Reverse(numArr);
+            Array.Resize(ref numArr, 4);
+            
+
+            for (int i = 0; i < numArr.Length; i++)
+            {
+                Console.WriteLine(numArr[i]);
+            }
+            int[] numArr2 = { 143, 20, 35 ,20};
+
+            Console.WriteLine(Array.IndexOf(numArr2, 20));
+            Console.WriteLine(Array.LastIndexOf(numArr2,20));
+
+            string text = "  Hello, World! ";
+
+            Console.WriteLine(text.ToLower());
+            Console.WriteLine(text.ToUpper());
+            Console.WriteLine(text.Trim());
+            Console.WriteLine(text.Replace(' ','-'));
+            Console.WriteLine(text.Replace('H', 'A'));
+            Console.WriteLine(text.Replace("World","PB302"));
+            Console.WriteLine(text);
+            Console.WriteLine(text.Contains("Worl "));
+            Console.WriteLine(text.Trim().ToUpper().StartsWith("HEL"));
+            Console.WriteLine(text.EndsWith("d! "));
+
+            string str = "Salam  necesen   ne var ne yox";
+
+            Console.WriteLine(str.IndexOf("nece"));
+            Console.WriteLine(str.IndexOf('a'));
+            Console.WriteLine(str.LastIndexOf('a'));
+
+            var newStr = str.Substring(1,2);
+            Console.WriteLine(newStr);
+
+            var arr = str.Split(' ');
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                Console.WriteLine(arr[i]);
+            }
+
+            var newStr2 = String.Join('-', arr);
+            Console.WriteLine(newStr2);
+
+            char letter = 'a';
+
+            Console.WriteLine(char.ToUpper('a'));
+            Console.WriteLine(char.IsDigit('a'));
+            Console.WriteLine(char.IsUpper('a'));
+            Console.WriteLine(char.IsLetter('a'));
+            Console.WriteLine(char.IsLetterOrDigit('a'));
+
+
+        }
+
+    }
+}
---
+++ 07012024/Lesson/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+
+namespace Lesson
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //int num = 3119;
+            //bool isPrime = true;
+            //int count = 0;
+
+            //for (int i = 2; i*i <= num; i++)
+            //{
+            //    count++;
+            //    if (num % i == 0)
+            //    {
+            //        isPrime = false;
+            //        break;
+            //    }
+            //}
+
+            //Console.WriteLine(isPrime);
+            //Console.WriteLine(count);
+
+            //int[] nums = { 12, 13, 14, 15, 20, 21, 22, 23, 24, 25, 40, 45, 67, 68, 69, 78, 70, 100, 101 };
+            //int num = 12;
+            //int index = -1;
+            //int count = 0;
+            //for (int i = 0; i < nums.Length; i++)
+            //{
+            //    count++;
+            //    if (nums[i] == num)
+            //    {
+            //        index = i;
+            //        break;
+            //    }
+            //}
+
+            int[] nums = { 12, 13, 14, 15, 20, 21, 22, 23, 24, 25, 40, 45, 67, 68, 69, 78, 70, 100, 101 };
+            int num = 67;
+            int index = -1;
+            int count = 0;
+
+            int min = 0;
+            int max = nums.Length - 1;
+
+            for (int i = min; min <= max; i++)
+            {
+                count++;
+                var mid = (min + max) / 2;
+
+                if (nums[mid] == num)
+                {
+                    index = mid;
+                    break;
+                }
+                else if (nums[mid]>num)
+                {
+                    max = mid - 1;
+                }
+                else
+                {
+                    min = mid + 1;
+                }
+            }
+
+
+
+            Console.WriteLine(index);
+            Console.WriteLine(count);
+        }
+    }
+}
